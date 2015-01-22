0ceae231600f63400cb70cd42010c6cb316712f4
zpopovic70 <popovicz70@gmail.com>
2015-01-22 18:44:42 +0000

    Fix to handle new category creation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,7 +25,12 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
+    @category = case params[:id]
+                when nil
+                  Category.new
+                else
+                  Category.find(params[:id])
+                end
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
